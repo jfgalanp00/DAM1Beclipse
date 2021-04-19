7d97dee7f80487dfae569636197e30f71e6ebd19
alumno <juanf.galper@educa.jcyl.es>
2021-04-19 16:59:23 +0200

    validacion numero 2 en cambio2 proyecto2

--- prueba1/src/cambio1/Cambio2.java
+++ prueba1/src/cambio1/Cambio2.java
@@ -4,7 +4,7 @@ public class Cambio2 {
 
 	public static void main(String[] args) {
 		System.out.println("Estamos en cambio2 preyecto2 ");
-
+		System.out.println("segunda linea en cambio2 preyecto2 ");
 	}
 
 }
