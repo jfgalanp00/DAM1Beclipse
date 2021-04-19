eee72c9a228a05718cd1d3482c2eab717fab31ef
alumno <juanf.galper@educa.jcyl.es>
2021-04-19 16:35:14 +0200

    validacion en cambio 1

--- prueba1/src/prueba/Principal.java
+++ prueba1/src/prueba/Principal.java
@@ -4,5 +4,6 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("hola mundo");
 		System.out.println("hola otra vez");
+		System.out.println("hola otra vez desde cambio1");
 	}
 }
